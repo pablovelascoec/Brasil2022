f052bcfc4633bcb3c14f16aa7a586f2b4e9ae436
Pablo Velasco-Oña <pablovelascoec@gmail.com>
2022-05-29 19:16:32 -0500

    spontaneous

--- Brasil2022v2.R
+++ Brasil2022v2.R
@@ -27,12 +27,12 @@ surveysbrnew$errinf <- surveysbrnew$votes - surveysbrnew$error
 
 mean_ends <-surveysbrnew %>%
   #selecting the last surveys
-  filter(date>"2022-04-24") %>% 
+  filter(date>"2022-05-01") %>% 
   group_by(candidate)
 data_ends <-aggregate(mean_ends$votes, list(mean_ends$candidate), FUN=mean, na.rm = TRUE)
 data_ends$voteper <- percent(data_ends$x)
 data_ends$candidate <- data_ends$Group.1
-data_ends$date <- as.Date("2022-05-18") 
+data_ends$date <- as.Date("2022-05-26") 
 
 
 ###this plot shows a smooth tendency and specific error margins calculated by the program 
@@ -74,7 +74,63 @@ surveysbrnew %>%
 #####Now sponteneous voting 
 
 
+spontbr <- read_excel("spontbr.xlsx")
+spontbr$Undecided_ <- spontbr$BN_ + spontbr$NR_
 
+spontbrnew <- spontbr %>%
+  #selecting relevant variables
+  select(date,survey,Lula_DaSilva,Jair_Bolsonaro,Outro_, Undecided_,N) %>%
+  #pivoteando
+  pivot_longer(cols = contains("_"),
+               names_to = "candidate", values_to = "votes")
+spontbrnew$date <- as.Date(spontbrnew$date) 
+spontbrnew$voteper <- percent(spontbrnew$votes)
+spontbrnew <- spontbrnew[order(spontbrnew$date), ]
+spontbrnew$error <- 1.96*sqrt(spontbrnew$votes*(1-spontbrnew$votes)/spontbrnew$N)
+spontbrnew$errsup <- spontbrnew$votes + spontbrnew$error
+spontbrnew$errinf <- spontbrnew$votes - spontbrnew$error
+
+mean_ends2 <-spontbrnew %>%
+  #selecting the last surveys
+  filter(date>"2022-05-01") %>% 
+  group_by(candidate)
+data_ends2 <-aggregate(mean_ends2$votes, list(mean_ends2$candidate), FUN=mean, na.rm = TRUE)
+data_ends2$voteper <- percent(data_ends2$x)
+data_ends2$candidate <- data_ends2$Group.1
+data_ends2$date <- as.Date("2022-05-26") 
+
+
+###this plot shows a smooth tendency and specific error margins calculated by the program 
+spontbrnew %>%
+  ggplot(aes(x = date, y = voteper, color=candidate, na.rm=TRUE)) +
+  geom_smooth( se=FALSE, size=1.1) +
+  stat_smooth(aes(fill=votes),method = "loess", size=1, alpha=0.1)+
+  scale_color_manual(values=c('darkblue', 'firebrick2', 'darkgoldenrod2', 'cornsilk4')) +
+  scale_fill_manual(values=c('darkblue', 'firebrick2', 'darkgoldenrod2', 'cornsilk4')) +
+  scale_linetype_manual(values=c('darkblue', 'firebrick2', 'darkgoldenrod2', 'cornsilk4')) +
+  geom_point() +
+  geom_text_repel(
+    aes(label = voteper), data = data_ends2,
+    fontface ="plain", color = "black", size = 3, 
+    force=1, point.padding=unit(1,'lines'),
+    vjust=1,
+    direction='y',
+    nudge_x=0.1,
+    segment.size=0.2,
+    na.rm = TRUE
+  ) + 
+  geom_vline(xintercept = as.numeric(as.Date("2022-10-02"))
+             , color = "black",
+             linetype="dashed",
+             alpha=0.8,
+             size=0.8)+
+  xlab(as.expression(expression( paste("Date") ))) +
+  ylab("Voting intention") +
+  scale_x_date(date_labels = "%Y-%m-%d") +
+  theme_fivethirtyeight() + 
+  labs(title = "Voting intention (spontaneous) - Brasil 2022", 
+       subtitle = "Consolidated evolution based on national surveys (Datafolha, Quaest, IPESPE)", 
+       caption = "OPALC - Sciences Po, by Pablo Velasco Oña") 
 
     
     
